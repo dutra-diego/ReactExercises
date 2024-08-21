8ae7d5a0cacea3e06c432bd09ab240607418b2fb
Diego Dutra <diiegodutra15@gmail.com>
2024-08-21 18:09:16 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { Header } from "./components/Header"
+import { Post } from "./components/Post"
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+import './global.css'
+function App() {
+
+
+  return (
+    <div>
+      <Header/>
+
+    
+    <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>     
+         <Post 
+         />
+      </main>
+
+    </div>
+    </div>
+   
+    
+  )
+}
+
+export default App
